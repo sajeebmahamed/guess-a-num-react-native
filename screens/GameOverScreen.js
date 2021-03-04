574acd8c3aca86a7cc28112cc617760359faef81
Sajeeb <sajeebahamed6@gmail.com>
2021-03-05 03:10:47 +0600

    work with imager

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,9 +1,17 @@
 import React from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, Image, StyleSheet, Text, View } from 'react-native';
 
 const GameOverScreen = ({ userNumber, roundsNumber, onRestart }) => (
+    /* eslint global-require: 0 */
     <View style={styles.screen}>
         <Text> The game is over! </Text>
+        <View style={styles.imageContainer}>
+            <Image
+                source={require('../assets/images/success.png')}
+                style={styles.image}
+                resizeMode="cover"
+            />
+        </View>
         <Text> Number of rounds: {roundsNumber} </Text>
         <Text> Number was: {userNumber} </Text>
         <Button title="NEW GAME" onPress={onRestart} />
@@ -15,6 +23,19 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
+    },
+    imageContainer: {
+        width: 300,
+        height: 300,
+        borderRadius: 150,
+        borderWidth: 3,
+        borderColor: 'black',
+        overflow: 'hidden',
+        marginVertical: 30,
+    },
+    image: {
+        width: '100%',
+        height: '100%',
     },
 });
 
